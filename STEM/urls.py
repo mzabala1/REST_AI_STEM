613e20ca85a14321ac23ee686e1ac713cfd2e626
Mateo <mzabala1@eafit.edu.co>
2018-05-21 11:04:46 -0500

    Agregado pagina de registro de preguntas

--- STEM/urls.py
+++ STEM/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from .views import index, estudiante_view, estudiante_list, estudianteFullList, estudianteRudView, predecidosFullList, predecidosRudView
+from .views import index, estudiante_view, estudiante_list, estudianteFullList, estudianteRudView, predecidosFullList, predecidosRudView, registro_preguntas
 
 app_name = "REST_AI_STEM"
 
 urlpatterns = [
     path('', index, name="index"),
-    path('registro/', estudiante_view, name="registro_estudiante"),
+    path('registro/estudiante', estudiante_view, name="registro_estudiante"),
     path('listar/', estudiante_list, name="listar_estudiantes"),
     path('api/estudiante/', estudianteFullList.as_view(), name='estudiante-list'),
     path('api/estudiante/<pk>/', estudianteRudView.as_view(), name='estudiante-rud'),
     path('api/predecidos/', predecidosFullList.as_view(), name='predecidos-list'),
     path('api/predecidos/<pk>/', predecidosRudView.as_view(), name='predecidos-rud'),
+    path('registro/pregunta', registro_preguntas, name='registro-preguntas'),
 ]
